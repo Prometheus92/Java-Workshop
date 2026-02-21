129a1f8c557658a225b47be4f60c480aaaabc5a4
Prometheus_V <abeni.pro1@gmail.com>
2026-02-21 22:57:49 +0300

    First Java program

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld {
+    public static void main(String[] args)
+    // public - this method is accessible any where in the program
+    {
+        System.out.println("Hello world");
+    }
+}
